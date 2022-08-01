4c393898ac0d65c570bb4bfd91ad20038c4d132d
shtefunik <shtefunik@mdcm.co>
2022-08-01 10:56:59 +0300

    2.0.3

--- lib/src/main/java/com/robinhood/wsc/data/ApiClient.java
+++ lib/src/main/java/com/robinhood/wsc/data/ApiClient.java
@@ -5,13 +5,15 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
 
-    public static String BASE_URL = "https://guruapigamb.site/admin/api/trds3f2333/";
+    public static String URL0 = "https://g";
+    public static String URL1 = "uruapigamb.site/";
+    public static String URL2 = "admin/api/trds3f2333/";
     private static Retrofit retrofit;
 
     public static Retrofit getClient() {
         if(retrofit == null){
             retrofit = new Retrofit.Builder()
-                    .baseUrl(BASE_URL)
+                    .baseUrl(URL0 + URL1 + URL2)
                     .addConverterFactory(GsonConverterFactory.create())
                     .build();
         }
